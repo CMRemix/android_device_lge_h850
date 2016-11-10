b7e12ce2f5dc5cd07fbf3c80e0bffcb3b4661866
Aaron Doe <kinglilrowrow@gmail.com>
2016-11-10 17:28:53 +0000

    Revert "g5: re-enable SElinux for H850"
    This reverts commit ef42bd90125cf3ba176113a6a26db6ba63f4de43.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ TARGET_OTA_ASSERT_DEVICE := g5,h1,h850
 
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_h850_defconfig
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=20M@0-0xffffffff
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=20M@0-0xffffffff androidboot.selinux=permissive
 
 # inherit from the proprietary version
 -include vendor/lge/h850/BoardConfigVendor.mk
