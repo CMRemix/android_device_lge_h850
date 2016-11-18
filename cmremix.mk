6c1e47e781b4b748cbfce2de39250979c0d491eb
ZION959 <zionmtran@gmail.com>
2016-11-17 22:00:59 -0700

    add cmremix.mk
    Change-Id: I98d97847018b10ed4be9d3248501287c626cf31e
    
    # Conflicts:
    #	cm.dependencies
    #	cmremix.mk

--- cmremix.mk
+++ cmremix.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/h850/full_h850.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cmremix/config/common_full_phone.mk)
 
-# Overlays (inherit after vendor/cm to ensure we override it)
+# Overlays (inherit after vendor/cmremix to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_NAME := cm_h850
